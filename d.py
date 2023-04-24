95a84f438833f69ab9934f089af69cfa30ed07e5
Гера <122350472+gera0lkndf@users.noreply.github.com>
2023-04-25 01:56:00 +0300

    токен добавил

--- d.py
+++ d.py
@@ -1,6 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
-from telegram.ext import CallbackQueryHandler, CommandHandler, ConversationHandler, Application
-from config import BOT_TOKEN
+from telegram.ext import CallbackQueryHandler, CommandHandler, ConversationHandler, Application, MessageHandler, filters
+
+BOT_TOKEN = "6292774773:AAHQnhCfZpJFNjJ5y9xFWaHpkII3f2HQ07c"
 
 
 reply_keyboard = [['/post', '/help']]
@@ -17,16 +18,20 @@ async def start_command(update, context):
 
 
 async def help_command(update, context):
-    await update.message.reply_text("Я пока не умею помогать... ")
-
-
-async def post(update, context):
-    keyboard = [[InlineKeyboardButton("Кнопка 1", callback_data='button1'),
-                 InlineKeyboardButton("Кнопка 2", callback_data='button2'),
-                 InlineKeyboardButton("Кнопка 3", callback_data='button3')]]
+    user = update.message.from_user.username
+    await update.message.reply_text(f"Привет {user}!", reply_markup=markup)
+    keyboard = [[InlineKeyboardButton("start", callback_data='a'),
+                 InlineKeyboardButton("stop", callback_data='b')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    with open('photo.jpg', 'rb') as photo:
+
+async def game(update, context):
+    keyboard = [[InlineKeyboardButton("Выйти в правый корридор", callback_data='button1'),
+                 InlineKeyboardButton("Зайти в левый корридор", callback_data='button2'),
+                 InlineKeyboardButton("Изучить дверь", callback_data='button3')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    with open('img.png', 'rb') as photo:
         await context.bot.send_photo(chat_id=update.message.chat_id, photo=photo, caption='Текст поста',
                                      reply_markup=reply_markup)
 
@@ -37,17 +42,32 @@ async def button(update, context):
     query = update.callback_query
     await query.answer()
 
-    keyboard = [[InlineKeyboardButton("Кнопка 1", callback_data='button1'),
-                 InlineKeyboardButton("Кнопка 2", callback_data='button2'),
-                 InlineKeyboardButton("Кнопка 3", callback_data='button3')]]
+    keyboard = [[InlineKeyboardButton("Выйти в правый корридор", callback_data='button1'),
+                 InlineKeyboardButton("Выйти в левый корридор", callback_data='button2'),
+                 InlineKeyboardButton("Изучить дверь", callback_data='button3')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     if query.data == 'button1':
-        with open('photo2.jpg', 'rb') as photo:
-            await context.bot.send_photo(chat_id=query.message.chat_id, photo=photo, caption='Другой текст',
+        with open('img1.jpg', 'rb') as photo:
+            await context.bot.send_photo(chat_id=query.message.chat_id, photo=photo, caption='первый вариант',
+                                         reply_markup=reply_markup)
+    if query.data == 'button2':
+        with open('img2.jpg', 'rb') as photo:
+            await context.bot.send_photo(chat_id=query.message.chat_id, photo=photo, caption='второй вариант',
+                                         reply_markup=reply_markup)
+    if query.data == 'button3':
+        with open('img3.jpg', 'rb') as photo:
+            await context.bot.send_photo(chat_id=query.message.chat_id, photo=photo, caption='третий вариант',
                                          reply_markup=reply_markup)
 
     return FIRST
+
+
+async def user_message(update, context):
+    with open('all_messages.txt', 'a', encoding='utf-8') as file:
+        user = update.message.from_user.username
+        text = update.message.text
+        file.write(f"@{user}: {text}\n")
 
 
 def main():
@@ -55,17 +75,18 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start_command)],
         states={
-            FIRST: [CommandHandler('post', post)],
+            FIRST: [CommandHandler('start_game', game)],
             SECOND: []
         },
         fallbacks=[]
     )
 
     application.add_handler(conv_handler)
+    text_handler = MessageHandler(filters.TEXT, user_message)
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CallbackQueryHandler(button))
-
+    application.add_handler(text_handler)
     application.run_polling()
     application.idle()
 
